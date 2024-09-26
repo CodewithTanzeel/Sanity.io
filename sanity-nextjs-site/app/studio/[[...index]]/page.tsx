fdca45f26b31be8ba387bdb48289d3f87852d6df
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-26 13:12:00 +0500

    .

---
+++ sanity-nextjs-site/app/studio/[[...index]]/page.tsx
@@ -0,0 +1,11 @@
+
+// app/studio/[[...index]]/page.tsx
+
+"use client";
+
+import { NextStudio } from "next-sanity/studio";
+import config from "@/sanity.config";
+
+export default function Studio() {
+  return <NextStudio config={config} />;
+}
