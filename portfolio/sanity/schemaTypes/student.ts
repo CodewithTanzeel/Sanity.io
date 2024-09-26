d568f35898f856a908e295f36449eb18b4a5b704
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-26 22:50:10 +0500

    .

---
+++ portfolio/sanity/schemaTypes/student.ts
@@ -0,0 +1,17 @@
+export const student = {
+  name: "student",
+  type: "document",
+  title: "Student Profile",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Student Name",
+    },
+    {
+      name: "age",
+      type: "string",
+      title: "Student Age",
+    },
+  ],
+};
